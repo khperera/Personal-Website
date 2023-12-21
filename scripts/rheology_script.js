3bf937b72f85cba5ba1775c46c7a108f7f1b087b
khperera <khperera@ncsu.edu>
2023-12-21 13:10:19 -0500

    updated, added harmonic

--- scripts/rheology_script.js
+++ scripts/rheology_script.js
@@ -6,23 +6,27 @@ function setCanvasSize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     firstharmonic = firstHarmonicPhase.value
-    drawPoints(generatePoints(10, canvas.width, canvas.height, firstharmonic),"blue");
+    drawPoints(generatePoints(100, canvas.width, canvas.height, firstHarmonicPhase.value),"blue", true);
+    drawPoints(generatePoints(100, canvas.width, canvas.height, 0),"black",false);
 }
 
 function generatePoints(i, n, m, firstharmonic) {
     const points = [];
 
     for (let count = 0; count < i; count++) {
-        const x = count/i * n;
-        const y = count/i * m;
+        const x =count/i * n;
+        const y = Math.sin(count/i*3.14*2+3.14+firstharmonic/180*3.14)*m/4+m/4;
         points.push({ x, y });
     }
 
     return points;
 }
 
-function drawPoints(points, color) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawPoints(points, color, clear) {
+    if (clear){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+        
 
     points.forEach(point => {
         ctx.beginPath();
@@ -33,6 +37,15 @@ function drawPoints(points, color) {
     });
 }
 
+
+// Add event listener to the slider input
+const slider = document.getElementById('slider');
+const selectedValue = document.getElementById('selectedValue');
+
+slider.addEventListener('input', function() {
+    // Update the displayed value as the slider is moved
+    setCanvasSize();
+});
 
 
 window.addEventListener('resize', setCanvasSize);
