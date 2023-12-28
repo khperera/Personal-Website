b2afd5155509e2437717b39878bc2e6fec0a21fc
khperera <126294394+ltnflash@users.noreply.github.com>
2023-12-28 00:24:10 -0500

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,35 @@
-const myImage = document.querySelector("img");
+// Include anime.js in your HTML before this script
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/catPic.jpg") {
-    myImage.setAttribute("src", "images/youtubeThumbnail.png");
-  } else {
-    myImage.setAttribute("src", "images/catPic.jpg");
-  }
-};
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+// Define the animation
+anime({
+  targets: '#wave',
+  d: [
+    { value: 'M10,100 Q50,50 90,100 T170,100' }, // starting shape
+    { value: 'M10,100 Q50,0 90,100 T170,100' }, // bigger sine wave
+    { value: 'M10,100 Q50,50 90,100 T170,100' }  // back to starting shape
+  ],
+  stroke: [
+    { value: '#FCA311' }, // starting color
+    { value: '#EB5111' }, // starting color
+    { value: '#FCA311' }   // ending color
+  ],
+  easing: 'easeInOutSine',
+  duration: 5000,
+  loop: true
+});
 
 
-
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
-  }
-
-
-if (!localStorage.getItem("name")) {
-setUserName();
-} else {
-const storedName = localStorage.getItem("name");
-myHeading.textContent = `Mozilla is cool, ${storedName}`;
-}
-
-myButton.onclick = () => {
-    setUserName();
-  };
+anime({
+  targets: '#wave2',
+  d: [
+    { value: 'M10,100 C23,75 37,50 50,50 C63,50 77,75 90,100 C103,125 117,150 130,150 C143,150 157,125 170,100'  },  // Single sine wave
+    { value: 'M10,100 C23,50 37,50 50,100 C63,150 77,150 90,100 C103,50 117,50 130,100 C143,150 157,150 170,100' } // Three sine waves
+  ],
+  stroke: [
+    { value: '#6495ED' }, // starting color
+    { value: '#FF6347' }  // ending color
+  ],
+  easing: 'easeInOutSine',
+  duration: 5000,
+  loop: true
+});
